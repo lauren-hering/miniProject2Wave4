e998161c8745059e4a0f6a3706ac3880de4b7e16
lauren <lauren.hering@comcast.net>
2020-07-23 10:50:10 -0500

    Initial Commit

---
+++ miniProject2Wave4/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  miniProject2Wave4
+//
+//  Created by Lauren Hering on 7/23/20.
+//  Copyright © 2020 Lauren Hering. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ miniProject2Wave4Tests/miniProject2Wave4Tests.swift
@@ -0,0 +1,34 @@
+//
+//  miniProject2Wave4Tests.swift
+//  miniProject2Wave4Tests
+//
+//  Created by Lauren Hering on 7/23/20.
+//  Copyright © 2020 Lauren Hering. All rights reserved.
+//
+
+import XCTest
+@testable import miniProject2Wave4
+
+class miniProject2Wave4Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
